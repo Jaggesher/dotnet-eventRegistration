7964b3a5e3faf82d0b9ceee3b3f04b4f0a9f7b4e
Jaggesher Mondal <jaggesher14@gmail.com>
2018-03-10 15:57:54 +0600

    Services Added

---
+++ Services/IStudentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_eventRegistration.Models;
+
+namespace dotnet_eventRegistration.Services
+{
+    public interface IStudentService
+    {
+        Task<bool>addStudent(StudentViewModel student);
+        Task<IEnumerable<Student>> getAll();
+
+     }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_eventRegistration.Models;
+using dotnet_eventRegistration.Data;
+
+namespace dotnet_eventRegistration.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly ApplicationDbContext _context;
+        public StudentService( ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> addStudent(StudentViewModel student)
+        {
+            var newStudent = new Student
+            {
+                Id = new Guid(),
+                Name = student.Name,
+                RollNo = student.RollNo,
+                FathersName = student.FathersName,
+                BirthDate = student.BirthDate,
+                Email = student.Email,
+                Phone = student.Phone,
+                Address = student.Address,
+                EduQualification = student.EduQualification
+            };
+
+            _context.Add(newStudent);
+
+            var result = await _context.SaveChangesAsync();
+
+            return result == 1 ;
+
+        }
+
+        public async Task<IEnumerable<Student>> getAll()
+        {
+            return _context.Students.ToList();
+
+        }
+    }
+}
